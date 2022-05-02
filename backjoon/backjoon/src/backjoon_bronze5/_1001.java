44852eb345809d0ff477b7d389a2d141a3ce14cd
Sungbin00 <103156250+Sungbin00@users.noreply.github.com>
2022-05-02 21:51:48 +0900

    Add files via upload

---
+++ backjoon/backjoon/src/backjoon_bronze5/_1001.java
@@ -0,0 +1,15 @@
+package backjoon_bronze5;
+
+import java.util.Scanner;
+
+public class _1001 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+        int A = scan.nextInt();
+        int B = scan.nextInt();
+        System.out.println(A-B);
+
+	}
+
+}
---
+++ backjoon/backjoon/src/backjoon_bronze5/_1271.java
@@ -0,0 +1,16 @@
+package backjoon_bronze5;
+
+import java.util.*;
+
+public class _1271 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		long n = scan.nextInt();
+		long m = scan.nextInt();
+		System.out.println(n/m);
+		System.out.println(n%m);
+
+	}
+
+}
