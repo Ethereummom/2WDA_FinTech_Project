8cd1b83480c02216e5e61ce9e789d07bf482b940
tkdrms4585 <tkdrms4585@naver.com>
2022-05-04 19:37:36 +0900

    Q2798

---
+++ src/backjoon_insung/Q2798_Insung.java
@@ -0,0 +1,36 @@
+package backjoon_insung;
+
+import java.util.Scanner;
+
+public class Q2798_Insung {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		// n개 만큼의 배열 생성
+		byte n = sc.nextByte();
+		int[] arr = new int[n];
+		// 목표수 m 입력
+		int m = sc.nextInt();
+		// 배열에 수 넣기
+		for (int i = 0; i < n; i++) {
+			arr[i] = sc.nextInt();
+		}
+
+		int max = 0, sum = 0;
+		a : for (int i = 0; i < n - 2; i++) {
+			for (int j = i + 1; j < n - 1; j++) {
+				for (int k = j + 1; k < n; k++) {
+					sum = arr[i] + arr[j] + arr[k];
+					if (sum == m) {
+						max = sum;
+						break a;
+					}
+					if (sum > max && sum < m) {
+						max = sum;
+					}
+					sum = 0;
+				}
+			}
+		}
+		System.out.println(max);
+	}
+}
