7b1030d31fc5c4bd303921e39df3909b462beb42
Ethereummom <sanghee_ok@naver.com>
2022-05-04 17:55:47 +0900

    Create web_scroll1_SH.py

---
+++ web_scroll1_SH.py
@@ -0,0 +1,27 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def get_per(code):
+    url = "https://finance.naver.com/item/main.nhn?code=" + code
+    html = requests.get(url).text
+
+    soup = BeautifulSoup(html, "html5lib")
+    tags = soup.select("#_per")
+    tag = tags[0]
+    return float(tag.text)
+
+def get_dividend(code) :
+    url = "https://finance.naver.com/item/main.nhn?code=" + code
+    html = requests.get(url).text
+
+    soup = BeautifulSoup(html, "html5lib")
+    tags = soup.select("#_dvr")
+    tag = tags[0]
+    return float(tag.text)
+
+code_number = input("원하는 주식의 종목 코드 번호를 입력해주세요")
+
+print(get_per(code_number))
+print(get_dividend(code_number))
+
