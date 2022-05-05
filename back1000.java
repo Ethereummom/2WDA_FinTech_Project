277880cd9e6d67d96f8fa9327221d5f2f421a4c8
jjun9 <103375871+jjun9@users.noreply.github.com>
2022-05-06 03:27:02 +0900

    basic test
    confirmation test

---
+++ back1000.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class back1000 {
+    public static void main(String[] args) {
+        /**
+         *  백준 문제 1000번
+         */
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(a+b);
+    }
+}
