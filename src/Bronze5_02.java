f375d643efab4af884c9c5934444ad7b6e1be368
CITYPOPER <103362619+CITYPOPER@users.noreply.github.com>
2022-05-05 19:31:24 +0900

    Create Bronze5_02.java

---
+++ src/Bronze5_02.java
@@ -0,0 +1,20 @@
+
+
+import java.util.Scanner;
+
+public class Bronze5_02 {
+
+	public static void main(String[] args) {
+		int a;
+		int b;
+		int sum;
+		
+		Scanner sc = new Scanner(System.in);
+		a = sc.nextInt();
+		b = sc.nextInt();
+		
+		sum = a-b;
+		System.out.println(sum);
+	}
+
+}
