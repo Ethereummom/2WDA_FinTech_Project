f746778353aae4cc6f3664abb780ad29a0b6b347
Sungbin00 <ddusin0614@naver.com>
2022-05-15 22:24:33 +0900

    Create _1008.java

---
+++ backjoon/backjoon/src/bronze4/_1008.java
@@ -0,0 +1,16 @@
+package bronze4;
+
+import java.util.*;
+
+public class _1008 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		double A = scan.nextDouble();
+		double B = scan.nextDouble();
+		
+		System.out.println(A/B);
+
+	}
+
+}
