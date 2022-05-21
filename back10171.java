1c77a312b2e056fc2c01b48baf6b769b2d1dd5b2
jjun9 <gkex1818@gmail.com>
2022-05-21 21:09:04 +0900

    단계별로 풀어보기 간단한 문제

---
+++ back10171.java
@@ -0,0 +1,9 @@
+public class back10171 {
+    public static void main(String[] args) {
+        
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( \')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
---
+++ back2557.java
@@ -0,0 +1,6 @@
+public class back2557 {
+    public static void main(String[] args) {
+        
+        System.out.println("Hello World!");
+    }
+}
