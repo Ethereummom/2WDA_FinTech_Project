80960ec0f6974190f4bfb7899f32b48f26c6f86e
Ethereummom <sanghee_ok@naver.com>
2022-05-25 00:47:30 +0900

    백준알고리즘

---
+++ BAEKJJOON/ApRil10_4.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class ApRil10_4 {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int y = in.nextInt();
+		int z = y - 543;
+		System.out.println(z);
+		
+
+	}
+
+}
