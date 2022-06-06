7f1dfdb19548ac0a287e05e9e1a226f3e86797f0
Sungbin00 <ddusin0614@naver.com>
2022-06-06 21:24:16 +0900

    Create _1157.java

---
+++ backjoon/backjoon/src/bronze1/_1157.java
@@ -0,0 +1,14 @@
+package bronze1;
+
+import java.util.*;
+
+public class _1157 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String str = scan.next();
+		
+
+	}
+
+}
